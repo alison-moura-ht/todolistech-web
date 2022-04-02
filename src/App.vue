229551572c6bea9305c06f103ebcabe6be7bb899
Alison Moura <alison.oghino@gmail.com>
2022-04-02 18:19:38 -0400

    Iniciando front-endo com Vue + VueRouter + Vuetify

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <app-navbar></app-navbar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import AppNavbar from "./components/AppNavbar.vue"
+</script>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    
+  }),
+}
+</script>
